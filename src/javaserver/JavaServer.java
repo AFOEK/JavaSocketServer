f9bf29aed4b1e6b8590d11606eb31e72a5602f21
AFOEK <felix.montalfu@gmail.com>
2020-12-01 16:32:04 +0700

    Ver 0.0.1

---
+++ src/javaserver/JavaServer.java
@@ -0,0 +1,10 @@
+package javaserver;
+
+public class JavaServer {
+
+    public static void main(String[] args) {
+        MainFrm main = new MainFrm();
+        main.setVisible(true);
+    }
+    
+}
